369d181309671f7a2e80627740a499e1f5a61b17
Gus Ramirez <63802071+gusram01@users.noreply.github.com>
2025-08-14 12:00:51 -0600

    Revamp app styles with minimalistic design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,34 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Inter', 'system-ui', 'sans-serif'],
+			},
+		},
 	},
-	plugins: [],
+	plugins: [
+		function({ addUtilities }) {
+			addUtilities({
+				'.line-clamp-1': {
+					overflow: 'hidden',
+					display: '-webkit-box',
+					'-webkit-box-orient': 'vertical',
+					'-webkit-line-clamp': '1',
+				},
+				'.line-clamp-2': {
+					overflow: 'hidden',
+					display: '-webkit-box',
+					'-webkit-box-orient': 'vertical',
+					'-webkit-line-clamp': '2',
+				},
+				'.line-clamp-3': {
+					overflow: 'hidden',
+					display: '-webkit-box',
+					'-webkit-box-orient': 'vertical',
+					'-webkit-line-clamp': '3',
+				},
+			})
+		}
+	],
 }
