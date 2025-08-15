889d11a974470ef1f8cd92376c23d709102d1a42
Gus Ramírez <g.ram.bt@hotmail.com>
2025-08-15 13:00:26 -0600

    Refactor environment variable handling and update scripts
    - Replaced direct import of WEB_API_BASE_URL from astro:env/server with process.env.WEB_API_BASE_URL in multiple files for better environment variable management.
    - Updated package.json to include dotenv and pm2 dependencies, and modified the start script to use pm2-runtime for production.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,37 @@
-import { defineConfig, envField } from 'astro/config';
-import node from '@astrojs/node';
-import clerk from '@clerk/astro';
-
-import tailwind from '@astrojs/tailwind';
+import { defineConfig, envField } from "astro/config";
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: node({
-    mode: 'standalone'
+    mode: "standalone",
   }),
-  integrations: [clerk({
-    afterSignInUrl: '/bookmarks/new',
-    afterSignUpUrl: '/bookmarks/new'
-  }), tailwind()],
+  integrations: [
+    clerk({
+      signInFallbackRedirectUrl: "/bookmarks/new",
+      signUpFallbackRedirectUrl: "/bookmarks/new",
+    }),
+    tailwind(),
+  ],
   env: {
     schema: {
-      WEB_API_BASE_URL: envField.string({ context: "server", access: "secret", default: "http://localhost:8000" }),
-    }
-  }
+      PUBLIC_CLERK_PUBLISHABLE_KEY: envField.string({
+        context: "server",
+        access: "secret",
+        default: "",
+      }),
+      CLERK_SECRET_KEY: envField.string({
+        context: "server",
+        access: "secret",
+        default: "",
+      }),
+      WEB_API_BASE_URL: envField.string({
+        context: "server",
+        access: "secret",
+        default: "http://localhost:8000",
+      }),
+    },
+  },
 });
